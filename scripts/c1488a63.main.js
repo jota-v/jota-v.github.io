8e5c4056d67488712244e278092727cab4e58799
jotav <juanvanni@gmail.com>
2013-09-06 01:04:53 -0300

    about

---
+++ scripts/c1488a63.main.js
@@ -0,0 +1,1 @@
+App=Ember.Application.create(),App.Router.map(function(){this.resource("about"),this.resource("portfolio"),this.resource("contact")}),App.WeaponsView=Ember.View.extend({didInsertElement:function(){$(".weapons li").tooltip()}});
