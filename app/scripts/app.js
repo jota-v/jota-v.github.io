945ed703f01d8014a09c4f393f64bc536e9c83b5
jotav <juanvanni@gmail.com>
2013-09-06 00:58:34 -0300

    about

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,6 +3,11 @@ App = Ember.Application.create();
 App.Router.map(function() {
     this.resource('about');
     this.resource('portfolio');
+    this.resource('contact');
 });
 
-
+App.WeaponsView = Ember.View.extend({
+    didInsertElement: function() {
+        $('.weapons li').tooltip();
+    }
+});
