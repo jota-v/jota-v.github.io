b285f93fa6840f8678ce97708dcb8e23968c20c6
jotav <juanvanni@gmail.com>
2013-09-08 20:09:59 -0300

    Deploy 1

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,7 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
+    this.resource('home', {path: '/'});
     this.resource('about');
     this.resource('portfolio');
     this.resource('contact');
@@ -11,3 +12,29 @@ App.WeaponsView = Ember.View.extend({
         $('.weapons li').tooltip();
     }
 });
+
+App.WorksView = Ember.View.extend({
+    didInsertElement: function() {
+        $('.works').gridalicious({
+            gutter: 40,
+            width: 220
+        });
+    }
+});
+
+App.PortfolioRoute = Ember.Route.extend({
+    model: function() {
+        var url = 'scripts/data.json';
+        return $.getJSON(url).then(function(data) {
+            return data.posts;
+        });
+    }
+});
+
+var showdown = new Showdown.converter();
+
+Ember.Handlebars.helper('format-markdown', function(input) {
+    return new Handlebars.SafeString(showdown.makeHtml(input));
+});
+
+
